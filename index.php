37c8e04bee1530fb511680b42b6f827f5b695e75
diwii <valdis.locs@gmail.com>
2015-10-08 06:28:39 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Viksieni - No mūsu ogām, Jūsu priekam.</title>
+  <!-- Include meta tag to ensure proper rendering and touch zooming -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- Include jQuery Mobile stylesheets -->
+  <link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css">
+  <!-- Include the jQuery library -->
+  <script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
+  <!-- Include the jQuery Mobile library -->
+  <script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
+  
+</head>
+<body>
+
+<div data-role="page" id="pageone" data-title="Viksieni - No mūsu ogām, Jūsu priekam.">
+  <div data-role="header">
+<h1 style="font-size:1.2em; padding: .2em 0; margin: 0 10%;">Viksieni</h1><h2 style="font-weight: normal; white-space: normal; margin: 0 10%; padding: .2em 0;">No mūsu ogām - Jūsu priekam!</h2>
+  </div>
+
+  <div data-role="main" class="ui-content">
+  <p style="font-size:2.5em; text-align:center;">Dabīgi sīrupi, pastilas, sinepes, eļļa no mūsu ogām un augiem.</p>
+  </div>
+
+  <div data-role="footer">
+  <h1 style="white-space: normal;">Kuldīgas novads, Rendas pagasts "Viļņi" PVD reģ.nr. 050637, mob. nr. +371 26 437 844, M. Simanoviča</h1>
+  </div>
+</div> 
+
+</body>
+</html>
